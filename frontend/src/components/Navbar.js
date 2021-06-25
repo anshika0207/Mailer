9b48a847cfd49afc4f07e625fd66d8b4e7f77873
Pooja Paliwal <60742005+poojapaliwal@users.noreply.github.com>
2021-06-25 19:07:08 +0530

    added frontend-login url

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div className="app__navbar">
+           
+        </div>
+    )
+}
+
+export default Navbar
